340e307e2e7f71d019496013fb2da026d247749f
ksuganya96 <ksuganya5896@gmail.com>
2025-07-24 14:30:53 +0530

    second commit

--- src/test/java/samptestng/group1.java
+++ src/test/java/samptestng/group1.java
@@ -3,15 +3,18 @@ package samptestng;
 import org.testng.annotations.Test;
 
 public class group1 {
-@Test(groups= {"sugu","nishan"})
+@Test(groups= {"sugu","nishanth"})
 public void test1()
 {
 	System.out.println("sun group1");
 }
-@Test(groups= {"nishu"})
+@Test(groups= {"nishanth"})
 public void test2()
 {
 	System.out.println("moon group1");
 }
-
+public void test3()
+{
+	System.out.println("saturn group1");
+}
 }
